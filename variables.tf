8d1480a8bb79e455bd53b0a1507aa79890e74a9c
Titouan-Esc <titouan.esc@icloud.com>
2025-07-25 23:43:39 +0200

    Closing GYM-13 Add key_name sensible + value in.gitignore

--- variables.tf
+++ variables.tf
@@ -19,19 +19,29 @@ variable "private_subnet_cidrs" {
   default = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
+
 variable "key_name" {
   type = string
   sensitive = true
   default = "SSH key name"
 }
 
-variable "instance_type" {
-  type = string
-  default = "t3.micro"
-}
-
 variable "db_password" {
   type        = string
   sensitive   = true
   description = "Mot de passe PostgreSQL"
+}
+
+variable "aws_id" {
+  type = string
+  sensitive = true
+}
+
+variable "aws_secret" {
+  type = string
+  sensitive = true
 }
