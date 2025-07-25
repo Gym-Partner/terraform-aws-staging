1c31965111a6248fec2f32b8284f795ee664a079
Titouan-Esc <titouan.esc@icloud.com>
2025-07-25 22:27:38 +0200

    Part of GYM-12 Add ec2 resources

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ module "vpc" {
   public_subnets_cidr = var.public_subnet_cidrs
   vpc_cidr = var.vpc_cidr
 }
+
+module "ec2" {
+  source = "./ec2"
+  vpc_id = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+  instance_type = var.instance_type
+  key_name = var.key_name
+}
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,3 +1,7 @@
 output "vpc_id" {
   value = aws_vpc.gym-partner-vpc.id
 }
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
